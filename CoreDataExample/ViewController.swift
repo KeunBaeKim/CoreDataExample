4cfec7d279dc787d5e47f59e11f0745262757280
TomKim <sakura1889@gmail.com>
2022-11-24 00:12:54 +0900

    add all
    add all

---
+++ CoreDataExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  CoreDataExample
+//
+//  Created by Keunbae Kim on 2022/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
